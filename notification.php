8c2115338f485ce1a7a4bd27a2adcaade0a91470
jssbaffi <jssbaffi@gmail.com>
2020-05-07 15:41:11 -0300

    Corrección de metodos en notificacion

--- notification.php
+++ notification.php
@@ -4,16 +4,16 @@ require_once "MercadoPagoService.php";
 
 switch($_POST["type"]) {
     case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
         break;
     case "plan":
-        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
         break;
     case "subscription":
-        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        $plan = MercadoPago\Subscription::find_by_id($_POST["id"]);
         break;
     case "invoice":
-        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        $plan = MercadoPago\Invoice::find_by_id($_POST["id"]);
         break;
 }
 

b0d4079a713d3668f9a01b73d970cfb3b1ea63ba
jssbaffi <jssbaffi@gmail.com>
2020-05-07 13:31:22 -0300

    Cambio de modal a checkout

--- detail.php
+++ detail.php
@@ -498,13 +498,14 @@
                                         <?php echo "$" . $_POST['unit'] ?>
                                     </h3>
                                 </div>
-<!--                                <a href="--><?php //print initPoint($_POST['title'], $_POST['price'], $_POST['unit'], $_POST['img']); ?><!--">Pay</a>-->
+                                <?php echo $_POST['img'] ?>
+                                <a href="<?php print initPoint($_POST['title'], $_POST['price'], $_POST['unit'], $_POST['img']); ?>">Pay</a>
 <!--                                <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>-->
-                                <form action="/procesar-pago" method="POST">
-                                    <script src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
-                                            data-preference-id="<?php print initPoint($_POST['title'], $_POST['price'], $_POST['unit'], $_POST['img']); ?>">
-                                    </script>
-                                </form>
+<!--                                <form action="/procesar-pago" method="POST">-->
+<!--                                    <script src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"-->
+<!--                                            data-preference-id="--><?php //print initPoint($_POST['title'], $_POST['price'], $_POST['unit'], $_POST['img']); ?><!--">-->
+<!--                                    </script>-->
+<!--                                </form>-->
                             </div>
                         </div>
                     </div>
